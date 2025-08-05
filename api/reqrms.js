546b34120118540938ce1a0d8b4a38be929841b5
durumee <durumee@gmail.com>
2025-08-05 11:13:14 +0900

    Update test data reqrms.js

--- api/reqrms.js
+++ api/reqrms.js
@@ -21,10 +21,20 @@ function toCamelCase(s) {
 // 모델 정보를 기반으로 테스트 데이터를 생성하는 함수
 function createTestData() {
   return [
-    // 1단계: 최상위 요구사항
+    // 1단계: 최상위 요구사항 - 2025년도 사업
+    {
+      REQRM_ID: 10,
+      UPPER_REQRM_ID: null,
+      REQRM_NM: '2025년도 사업',
+      REQRM_DC: '2025년 서비스 개선 및 신규 기능 도입을 위한 전반적인 요구사항',
+      SORT_ORDR: 1,
+      DEL_AT: 'N',
+    },
+
+    // 2단계: '2025년도 사업'(10)의 하위 요구사항 (기존의 최상위 항목들)
     {
       REQRM_ID: 100,
-      UPPER_REQRM_ID: null,
+      UPPER_REQRM_ID: 10,
       REQRM_NM: '사용자 관리',
       REQRM_DC: '서비스 이용을 위한 회원 기능 전반을 관리한다.',
       SORT_ORDR: 1,
@@ -32,7 +42,7 @@ function createTestData() {
     },
     {
       REQRM_ID: 200,
-      UPPER_REQRM_ID: null,
+      UPPER_REQRM_ID: 10,
       REQRM_NM: '상품 관리',
       REQRM_DC: '서비스에서 제공하는 상품 정보를 관리한다.',
       SORT_ORDR: 2,
@@ -40,14 +50,14 @@ function createTestData() {
     },
     {
       REQRM_ID: 300,
-      UPPER_REQRM_ID: null,
+      UPPER_REQRM_ID: 10,
       REQRM_NM: '결제 시스템',
       REQRM_DC: '상품 구매에 필요한 결제 기능을 제공한다.',
       SORT_ORDR: 3,
       DEL_AT: 'N',
     },
 
-    // 2단계: '사용자 관리'(100) 하위 요구사항
+    // 3단계: 기존의 2단계 항목들 (UPPER_REQRM_ID가 100, 200, 300으로 변경됨)
     {
       REQRM_ID: 101,
       UPPER_REQRM_ID: 100,
@@ -70,62 +80,10 @@ function createTestData() {
       REQRM_NM: '회원 정보 수정',
       REQRM_DC: '사용자가 자신의 정보를 수정하는 기능',
       SORT_ORDR: 3,
-      DEL_AT: 'Y', // 삭제된 기능 예시
+      DEL_AT: 'Y',
     },
 
-    // 3단계: '회원가입'(101) 하위 요구사항
-    {
-      REQRM_ID: 1011,
-      UPPER_REQRM_ID: 101,
-      REQRM_NM: '필수 정보 입력',
-      REQRM_DC: '이름, 이메일, 비밀번호 등 필수 정보를 입력받는다.',
-      SORT_ORDR: 1,
-      DEL_AT: 'N',
-    },
-    {
-      REQRM_ID: 1012,
-      UPPER_REQRM_ID: 101,
-      REQRM_NM: '유효성 검증',
-      REQRM_DC: '입력된 정보의 유효성을 검증한다.',
-      SORT_ORDR: 2,
-      DEL_AT: 'N',
-    },
-    {
-      REQRM_ID: 1013,
-      UPPER_REQRM_ID: 101,
-      REQRM_NM: '가입 완료 메일 발송',
-      REQRM_DC: '가입 완료 후 확인 메일을 발송한다.',
-      SORT_ORDR: 3,
-      DEL_AT: 'N',
-    },
-
-    // 3단계: '로그인'(102) 하위 요구사항
-    {
-      REQRM_ID: 1021,
-      UPPER_REQRM_ID: 102,
-      REQRM_NM: 'ID/PW 로그인',
-      REQRM_DC: '아이디와 비밀번호로 로그인한다.',
-      SORT_ORDR: 1,
-      DEL_AT: 'N',
-    },
-    {
-      REQRM_ID: 1022,
-      UPPER_REQRM_ID: 102,
-      REQRM_NM: '소셜 로그인',
-      REQRM_DC: 'SNS 계정을 통해 로그인한다.',
-      SORT_ORDR: 2,
-      DEL_AT: 'N',
-    },
-    {
-      REQRM_ID: 1023,
-      UPPER_REQRM_ID: 102,
-      REQRM_NM: '자동 로그인',
-      REQRM_DC: '사용자가 자동 로그인을 선택할 수 있다.',
-      SORT_ORDR: 3,
-      DEL_AT: 'N',
-    },
-
-    // 2단계: '상품 관리'(200) 하위 요구사항
+    // 3단계: '상품 관리'(200) 하위 요구사항
     {
       REQRM_ID: 201,
       UPPER_REQRM_ID: 200,
@@ -151,17 +109,7 @@ function createTestData() {
       DEL_AT: 'N',
     },
 
-    // 3단계: '상품 목록 조회'(201) 하위 요구사항
-    {
-      REQRM_ID: 2011,
-      UPPER_REQRM_ID: 201,
-      REQRM_NM: '필터링 기능',
-      REQRM_DC: '상품 카테고리, 가격 등으로 필터링할 수 있다.',
-      SORT_ORDR: 1,
-      DEL_AT: 'N',
-    },
-
-    // 2단계: '결제 시스템'(300) 하위 요구사항
+    // 3단계: '결제 시스템'(300) 하위 요구사항
     {
       REQRM_ID: 301,
       UPPER_REQRM_ID: 300,
@@ -178,8 +126,67 @@ function createTestData() {
       SORT_ORDR: 2,
       DEL_AT: 'N',
     },
+    
+    // 4단계: 기존의 3단계 항목들 (UPPER_REQRM_ID가 101, 102, 201 등으로 변경됨)
+    {
+      REQRM_ID: 1011,
+      UPPER_REQRM_ID: 101,
+      REQRM_NM: '필수 정보 입력',
+      REQRM_DC: '이름, 이메일, 비밀번호 등 필수 정보를 입력받는다.',
+      SORT_ORDR: 1,
+      DEL_AT: 'N',
+    },
+    {
+      REQRM_ID: 1012,
+      UPPER_REQRM_ID: 101,
+      REQRM_NM: '유효성 검증',
+      REQRM_DC: '입력된 정보의 유효성을 검증한다.',
+      SORT_ORDR: 2,
+      DEL_AT: 'N',
+    },
+    {
+      REQRM_ID: 1013,
+      UPPER_REQRM_ID: 101,
+      REQRM_NM: '가입 완료 메일 발송',
+      REQRM_DC: '가입 완료 후 확인 메일을 발송한다.',
+      SORT_ORDR: 3,
+      DEL_AT: 'N',
+    },
+    {
+      REQRM_ID: 1021,
+      UPPER_REQRM_ID: 102,
+      REQRM_NM: 'ID/PW 로그인',
+      REQRM_DC: '아이디와 비밀번호로 로그인한다.',
+      SORT_ORDR: 1,
+      DEL_AT: 'N',
+    },
+    {
+      REQRM_ID: 1022,
+      UPPER_REQRM_ID: 102,
+      REQRM_NM: '소셜 로그인',
+      REQRM_DC: 'SNS 계정을 통해 로그인한다.',
+      SORT_ORDR: 2,
+      DEL_AT: 'N',
+    },
+    {
+      REQRM_ID: 1023,
+      UPPER_REQRM_ID: 102,
+      REQRM_NM: '자동 로그인',
+      REQRM_DC: '사용자가 자동 로그인을 선택할 수 있다.',
+      SORT_ORDR: 3,
+      DEL_AT: 'N',
+    },
+    {
+      REQRM_ID: 2011,
+      UPPER_REQRM_ID: 201,
+      REQRM_NM: '필터링 기능',
+      REQRM_DC: '상품 카테고리, 가격 등으로 필터링할 수 있다.',
+      SORT_ORDR: 1,
+      DEL_AT: 'N',
+    },
   ];
 }
+
 
 
 // 서버리스 함수 핸들러
